52ccae68dabbf467a6321e041f43a03a092e20bb
Daniel Hernández <tu correo para identificarte en el repositorio>
2018-03-28 16:02:08 +0200

    creation of the database

---
+++ financial-control-server-app/src/main/resources/db/migration/V0_1__TableCreation.sql
@@ -0,0 +1,30 @@
+create table book_entry_types (
+  id number(1) primary key,
+  type_name varchar(10)
+);
+
+insert into book_entry_types(id, type_name) values (1, 'INCOME');
+insert into book_entry_types(id, type_name) values (2, 'EXPENSES');
+
+create table book_entries (
+  id number(10) auto_increment primary key,
+  concept varchar(255),
+  amount number(8,2),
+  type_id number(1),
+  punctual_entry boolean,
+  avoidable_enrty boolean,
+  foreign key (type_id) references book_entry_types(id)
+);
+
+create table fixed_book_entries (
+  id number(10) auto_increment primary key,
+  concept varchar(255),
+  amount number(8,2),
+  type_id number(1),
+  end_date date,
+  foreign key (type_id) references book_entry_types(id)
+);
+
+
+
+
