fe05b6576c6e203dfcd7513e2434841a215650de
Daniel Hernández <tu correo para identificarte en el repositorio>
2018-03-28 14:46:21 +0200

    Spring Initializr generated code

---
+++ src/main/java/es/dhernandez/financial/financialcontrolserver/FinancialControlServerApplication.java
@@ -0,0 +1,12 @@
+package es.dhernandez.financial.financialcontrolserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialControlServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialControlServerApplication.class, args);
+	}
+}
